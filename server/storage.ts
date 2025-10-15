f5e63e0e22ccbde295fb857c60a0b9e73e0ae886
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-10-16 00:38:11 +0530

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -1,38 +1,6 @@
-import { type User, type InsertUser } from "../shared/schema";
-import { randomUUID } from "crypto";
-
-// modify the interface with any CRUD methods
-// you might need
-
+// This file is no longer used - app uses Supabase directly
 export interface IStorage {
-  getUser(id: string): Promise<User | undefined>;
-  getUserByUsername(username: string): Promise<User | undefined>;
-  createUser(user: InsertUser): Promise<User>;
+  // Placeholder interface
 }
 
-export class MemStorage implements IStorage {
-  private users: Map<string, User>;
-
-  constructor() {
-    this.users = new Map();
-  }
-
-  async getUser(id: string): Promise<User | undefined> {
-    return this.users.get(id);
-  }
-
-  async getUserByUsername(username: string): Promise<User | undefined> {
-    return Array.from(this.users.values()).find(
-      (user) => user.username === username,
-    );
-  }
-
-  async createUser(insertUser: InsertUser): Promise<User> {
-    const id = randomUUID();
-    const user: User = { ...insertUser, id };
-    this.users.set(id, user);
-    return user;
-  }
-}
-
-export const storage = new MemStorage();
+export const storage = {} as IStorage;
